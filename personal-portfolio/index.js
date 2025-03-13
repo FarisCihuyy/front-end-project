55e0b5479e6fe4ce4530e58d1f1cbf2be670854a
Faris-Cihuy <farisngab78@gmail.com>
2025-03-13 09:11:39 +0700

    Personal-Portfolio

---
+++ personal-portfolio/index.js
@@ -0,0 +1,14 @@
+const theme = localStorage.getItem("theme");
+if (theme === "dark") {
+  document.body.classList.add("dark-mode");
+}
+
+const toggleBtnTheme = document.querySelector("#toggle-theme");
+toggleBtnTheme.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  const theme = document.body.classList.contains("dark-mode") ? "dark" : "light";
+
+  console.log(theme);
+
+  localStorage.setItem("theme", theme);
+});
